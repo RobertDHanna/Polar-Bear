654e491de36989f8c5aac3590331b5142a064030
Robert Hanna <robertdhanna94@gmail.com>
2017-07-19 19:46:14 -0600

    made view mobile friendly

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,31 +4,29 @@
 @endsection
 @section('content')
     <div class="container-fluid poll-main-body">
-        <div class="row">
-            <div class="col-sm-3"></div>
-            <div class="col-sm-6">
-                <h1 class="main-header">Create a poll</h1>
-                <form>
-                    <label>
-                        <input id="fname" type="text" placeholder="You can type your question here.">
-                        <span>Question</span>
-                    </label>
+            <div class="row">
+                <div class="col-md-5 col-md-offset-4">
+                    <h1 class="main-header">Create a poll</h1>
+                        <form class="poll">
+                            <label>
+                                <input id="fname" type="text" placeholder="You can type your question here.">
+                                <span>Question</span>
+                            </label>
 
-                    <label>
-                        <input class="poll-option-input" id="fname" type="text" placeholder="You can put an option here.">
-                        <span>Option</span>
-                    </label>
+                            <label>
+                                <input class="poll-option-input" id="fname" type="text" placeholder="You can put an option here.">
+                                <span>Option</span>
+                            </label>
 
-                    <label>
-                        <input class="poll-option-input" id="fname" type="text" placeholder="You can put an option here.">
-                        <span>Option</span>
-                    </label>
-                    
-                    <input type="submit" value="Create">
-                </form>
+                            <label>
+                                <input class="poll-option-input" id="fname" type="text" placeholder="You can put an option here.">
+                                <span>Option</span>
+                            </label>
+                            
+                            <input type="submit" value="Create">
+                        </form>
+                </div>
             </div>
-            <div class="col-sm-3"></div>
-        </div>
     </div>
 @endsection
 
