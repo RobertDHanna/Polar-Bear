eabdae2b48b94cc82a60f04973baecae1b899f2e
Robert Hanna <robertdhanna94@gmail.com>
2017-07-30 15:13:02 -0600

    added poll url attribute to Poll model

--- app/Poll.php
+++ app/Poll.php
@@ -8,11 +8,24 @@ use App\Option;
 class Poll extends Model
 {
     protected $fillable = ['question'];
-    protected $appends = ['options'];
+    protected $appends = ['options', 'poll_url'];
     protected $hidden = ['owner_id', 'password'];
 
     public function getOptionsAttribute()
     {
         return Option::where('poll_id', '=', $this->id)->get();
+    }
+
+    public function getPollUrlAttribute()
+    {
+        $endpoint = "/poll/$this->id";
+        if (env('APP_ENV') === 'local')
+        {
+            return url($endpoint);
+        }
+        else
+        {
+            return secure_url($endpoint);
+        }
     }
 }
