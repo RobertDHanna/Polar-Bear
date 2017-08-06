257304952ae6bb7d2cc2ab5ced749f0beec53ef9
Robert Hanna <robertdhanna94@gmail.com>
2017-08-06 16:20:52 -0600

    added google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,14 @@
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
     <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-104183891-1', 'auto');
+        ga('send', 'pageview');
+
         $(document).ready(function() {
             $('[data-toggle="tooltip"]').tooltip();
             $.ajaxSetup({
